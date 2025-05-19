e22390d597a1c015300379d72ef4036ba77438da
shba007 <shirsendu2001@gmail.com>
2025-05-19 21:43:03 +0530

    fix: update Typesense API key and command in docker-compose files

--- server/utils/typesense.ts
+++ server/utils/typesense.ts
@@ -1,8 +1,8 @@
 import Typesense from 'typesense'
 
 const typesense = new Typesense.Client({
-  nodes: [{ host: process.env.SEARCH_DATABASE_HOST!, port: parseInt(process.env.SEARCH_DATABASE_PORT!), protocol: 'http' }],
-  apiKey: process.env.SEARCH_DATABASE_KEY!,
+  nodes: [{ host: process.env.TYPESENSE_HOST!, port: process.env.TYPESENSE_PORT!, protocol: 'http' }],
+  apiKey: process.env.TYPESENSE_API_KEY!,
 })
 
 export default typesense
